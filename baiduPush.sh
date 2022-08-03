e556ee7cfa9d37b7d965ff4f88b1b2b4041cc7e0
sol <solmp163@163.com>
2022-08-03 13:04:11 +0800

    test

--- baiduPush.sh
+++ baiduPush.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env sh
 
+
 # 确保脚本抛出遇到的错误
 set -e
 
 echo $BaiduPush_TOKEN
 
-echo $GITHUB_TOKEN
+echo ${{BaiduPush_TOKEN}}
 
-echo env.GITHUB_TOKEN
+echo ${{GITHUB_TOKEN}}
+
+echo ${{secrets.GITHUB_TOKEN}}
+
+echo ${{env.SERVER_IP}}
+
 
 echo "end"
