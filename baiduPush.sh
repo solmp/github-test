980146d6fcc1ec0918f8309120632409a7228074
sol <solmp163@163.com>
2022-08-03 13:25:33 +0800

    1

--- baiduPush.sh
+++ baiduPush.sh
@@ -6,15 +6,16 @@ set -e
 
 echo "start"
 
-echo "BaiduPush_TOKEN:"
-echo ${BaiduPush_TOKEN}
-echo $GITHUB_TOKEN
-echo ${secrets.BaiduPush_TOKEN}
+echo "BAIDUPUSH_TOKEN-2: $BAIDUPUSH_TOKEN"
+echo "BAIDUPUSH_TOKEN-1: ${BAIDUPUSH_TOKEN}"
+echo "BAIDUPUSH_TOKEN-3: ${secrets.BAIDUPUSH_TOKEN}"
 
-echo ${GITHUB_TOKEN}
+echo "GITHUB_TOKEN-1: $GITHUB_TOKEN"
+echo "GITHUB_TOKEN-2: ${GITHUB_TOKEN}"
+echo "GITHUB_TOKEN-3: ${secrets.GITHUB_TOKEN}"
 
-echo ${secrets.GITHUB_TOKEN}
-
-echo ${env.SERVER_IP}
+echo "SERVER_IP-1: $SERVER_IP"
+echo "SERVER_IP-2: ${SERVER_IP}"
+echo "SERVER_IP-3: ${env.SERVER_IP}"
 
 echo "end"
